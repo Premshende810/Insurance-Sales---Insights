b13d1b3647d0c7eaed58e0f58abc3377a7bddc1b
Premshende810 <123981525+Premshende810@users.noreply.github.com>
2024-10-20 15:03:42 +0530

    Update vw_total_car_insurance_customer.sql

--- DBT/DBT_DEV/vw_total_car_insurance_customer.sql
+++ DBT/DBT_DEV/vw_total_car_insurance_customer.sql
@@ -18,6 +18,8 @@ WITH car_insurance AS (
         i.claim_id,
         i.claim_type,
         i.claim_date,
+        i.PAYMENT_AMOUNT,
+        i.SETTLEMENT_DATE,
         i.claim_status,
         p.DEDUCTIBLE,
         ROW_NUMBER() OVER (PARTITION BY c.cust_id ORDER BY p.ISSUED_DATE DESC) AS rn
@@ -45,6 +47,8 @@ SELECT
         claim_type,
         claim_date,
         claim_status,
+        PAYMENT_AMOUNT,
+        SETTLEMENT_DATE,
         DEDUCTIBLE,
         SUM(TERM_AMT) OVER (PARTITION BY agent_id) AS total_revenue
 FROM car_insurance
