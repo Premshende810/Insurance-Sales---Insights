9657112e51e0a1369790c648317f9f15a621b3f7
Premshende810 <123981525+Premshende810@users.noreply.github.com>
2024-10-20 14:57:55 +0530

    Update vw_total_house_insurance_customer.sql

--- Snowflake/DEV_ENV/DBT_DEV/VIEWS/vw_total_house_insurance_customer.sql
+++ Snowflake/DEV_ENV/DBT_DEV/VIEWS/vw_total_house_insurance_customer.sql
@@ -1,4 +1,26 @@
-{{ config(materialized = 'view') }}
+create or replace view DEV_ENV.DBT_DEV.VW_TOTAL_HOUSE_INSURANCE_CUSTOMER(
+	CUST_ID,
+	AGENT_ID,
+	POLICY_NUM,
+	FULL_NAME,
+	DOB,
+	GENDER,
+	COUNTRY,
+	TYPE,
+	COVERAGE,
+	TERM_AMT,
+	ISSUED_DATE,
+	PAYMENT_ID,
+	CLAIM_ID,
+	CLAIM_TYPE,
+	CLAIM_DATE,
+	CLAIM_STATUS,
+	PAYMENT_AMOUNT,
+	SETTLEMENT_DATE,
+	DEDUCTIBLE,
+	TOTAL_REVENUE
+) as (
+    
 WITH house_insurance AS (
     SELECT 
         c.cust_id, 
@@ -23,12 +45,12 @@ WITH house_insurance AS (
         i.claim_status,
         p.DEDUCTIBLE,
         ROW_NUMBER() OVER (PARTITION BY c.cust_id ORDER BY p.ISSUED_DATE DESC) AS rn
-    FROM {{ ref("dim_customer") }} AS c
-    INNER JOIN {{ ref("fact_policy") }} AS p ON c.policy_num = p.policy_num
-    JOIN {{ ref("fact_payment") }} AS t ON t.payment_id = p.payment_id
-    JOIN {{ ref("house") }} h ON h.policy_num = p.policy_num
-    JOIN {{ ref("dim_agent") }} a ON a.agent_id = c.agent_id
-    JOIN {{ ref("dim_claims") }} i ON i.policy_num = p.policy_num
+    FROM DEV_ENV.DBT_DEV.dim_customer AS c
+    INNER JOIN DEV_ENV.DBT_DEV.fact_policy AS p ON c.policy_num = p.policy_num
+    JOIN DEV_ENV.DBT_DEV.fact_payment AS t ON t.payment_id = p.payment_id
+    JOIN DEV_ENV.DBT_DEV.house h ON h.policy_num = p.policy_num
+    JOIN DEV_ENV.DBT_DEV.dim_agent a ON a.agent_id = c.agent_id
+    JOIN DEV_ENV.DBT_DEV.dim_claims i ON i.policy_num = p.policy_num
 )
 
  SELECT 
@@ -54,3 +76,4 @@ WITH house_insurance AS (
         SUM(TERM_AMT) OVER (PARTITION BY agent_id) AS total_revenue
     FROM house_insurance
     WHERE rn = 1
+  );
